d01d2b1ef81e2f84503b6ce98654ecf25ca59bff
Gaurav Cedric Bhatnagar <gaurav.cedric@gmail.com>
2015-09-14 17:48:36 -0700

    Adding tests for the errors package. Doesn't look like we can use CoffeeScript for tests... for now.

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -5,10 +5,17 @@ Package.describe({
   summary: 'A pattern to display application errors to the user'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
 
   api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
 });
+
+Package.onTest(function (api) {
+  api.use('gbhatnag:errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');
+
+  api.addFiles('errors_tests.js', 'client');
+})
